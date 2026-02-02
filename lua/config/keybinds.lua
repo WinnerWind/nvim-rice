732e3bcc7bf5b7a72b3d8d431a346b01e31bbd8b
WinnerWind <contact@winnerwind.in>
2026-02-02 09:30:59 +0530

    Tab bar and neotree

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -5,3 +5,33 @@ end
 keymap('<S-Up>', ':m-2<CR>')
 keymap('<S-Down>', ':m+<CR>')
 
+-- BarBar
+-- meta key = alt
+keymap('<M-Right>', ':tabnext<CR>')
+keymap('<M-Left>', ':tabprevious<CR>')
+keymap('<M-x>', ':BufferClose<CR>')
+keymap('<M-n>', ':tabnew<CR>')
+keymap('<M-p>', ':BufferPin<CR>')
+keymap('<M-X>', ':BufferRestore<CR>')
+keymap('<M->>', ':BufferCloseBuffersRight<CR>')
+keymap('<M-lt>', ':BufferCloseBuffersLeft<CR>')
+
+-- Move between splits with alt direction
+keymap('<C-S-Up>', ':wincmd k<CR>')
+keymap('<C-S-Down>', ':wincmd j<CR>')
+keymap('<C-S-Left>', ':wincmd h<CR>')
+keymap('<C-S-Right>', ':wincmd l<CR>')
+
+keymap('<M-Bslash>',':vsplit<CR>')
+keymap('<M-->',':split<CR>')
+
+keymap('<C-Up>', '<C-w>K')
+keymap('<C-Left>', '<C-w>H')
+keymap('<C-Down>', '<C-w>J')
+keymap('<C-Right>', '<C-w>L') 
+
+keymap('<M-[>', '<C-w><') -- decrease width
+keymap('<M-]>', '<C-w>>') -- increase width
+keymap('<M-{>', '<C-w>-') -- decrease height
+keymap('<M-}>', '<C-w>+') -- increase height
+
