75938745502a3b72e41ec38319db0cc8e6ecd0d1
WinnerWind <contact@winnerwind.in>
2026-02-03 12:50:07 +0530

    Add lualine

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ require("config.lsp")
 
 require("config.keybinds")
 require("config.flags")
+require("config.lualine")
 require("config.autoquit-neotree")
 
 require("config.sessions")
 -- Theme
 vim.cmd([[colorscheme gruvbox]])
-
