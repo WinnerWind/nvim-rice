d16541c8046c09b838109056bb5680e20aa84c22
WinnerWind <contact@winnerwind.in>
2026-02-06 16:46:20 +0530

    Misc. bar styling

--- lua/config/bar.lua
+++ lua/config/bar.lua
@@ -1,13 +1,28 @@
 return {
-			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-			-- animation = true,
-			-- insert_at_start = true,
-			-- …etc.
-			sidebar_filetypes = {
-				 ['neo-tree'] = {
+	-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+	-- animation = true,
+	-- insert_at_start = true,
+	-- …etc.
+	sidebar_filetypes = {
+		 ['neo-tree'] = {
 			event = 'BufWipeout',
 		},
+	},
+	icons = {
+		preset = 'default',
+		buffer_index = 'superscript',
+			diagnostics = {
+				[vim.diagnostic.severity.ERROR] = {enabled = true, icon},
+				[vim.diagnostic.severity.WARN] = {enabled = true},
+				[vim.diagnostic.severity.INFO] = {enabled = false},
+				[vim.diagnostic.severity.HINT] = {enabled = false},
 			},
+			gitsigns = {
+				added = {enabled = true},
+				changed = {enabled = true},
+				deleted = {enabled = true},
+			},
+	},
 }
 
 
