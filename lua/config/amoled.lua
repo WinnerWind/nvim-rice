a84fee600eb9a1c6100d3bc8eeb1a15d8a409654
WinnerWind <contact@winnerwind.in>
2026-02-24 09:27:38 +0530

    Use emperor theme
    custom colours not wokring

--- lua/config/amoled.lua
+++ lua/config/amoled.lua
@@ -1,6 +1,11 @@
 return function()
 	require("black-metal").setup({
-		theme = "bathory",
+		theme = "emperor",
+		colors = {
+			fg = "#f1f1f1",
+			comment = "#e1e1e1",
+			keyword = "#f1f1f1",
+		},
 	})
 	require("black-metal").load()
 end
